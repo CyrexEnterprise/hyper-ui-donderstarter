c0990b0d2c7990104744de392f9db7cc897426de
Rui Molefas <molefas@gmail.com>
2015-12-20 12:33:37 +0000

    v0.0.1 commit

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # UI-DonderStarter
 Boilerplate for general UI package development using gulp, sass and bower
+
+## Features
+- Gulp;
+- Bower to manage dependencies;
+- CSS pre-compile through SASS;
+- JShint;
+- SASS and JS minification and concatenation;
+- Handlebars for template concatenation;
+- Localhost server deploy;
+- Synchronized browser testing;
+- Atomic design system;
+
+## Requirements
+- [nodejs](https://nodejs.org/)
+- [npm](https://www.npmjs.com/)
+
+## Install
+Install the dependencies with `npm install` or `sudo npm install`. Then just run `gulp`.
+
+## Modular approach
+This UI boilerplate is based on atomic design methodology. The components are divided into **Atoms**, **Molecules**, **Organisms** & **Pages**, thus, using a modular development approach. More information on each component is found on their respective folders. There is also a **global** folder containing header & footer placeholders for your head & footer code, that will automatically be included on each page. More information on this design patter can be found [here](http://patternlab.io/about.html).
+
+## Development workflow overview
+- **Atoms** will include the smaller components;
+- **Molecules** are components that will include atom components;
+- **Organisms** are bigger components that will include molecules & atoms;
+- **Pages** are page templates that are built through the usage of all components put together. Will include the code from head.html & foot.html automatically
+- **head.html** is the placeholder for all head scripts, links and any code that should be placed on top of an HTML template;
+- **foot.html** is the placeholder for all the scripts that should be placed on the bottom of an HTML template;
+
+## Adding packages
+The donderstarters are setup so you can easily add external packages to your project, while making it easier to manage dependencies. Doing
+`bower install <package-name>` will add the package to the src/vendor folder. You can browse for available bower packages through [bower search page](http://bower.io/search/), and general documentation on bower in [bower.io](http://bower.io/).
+
+After adding the package, you need to include it into the build process (gulpfile.js:44) and into your foot.html file, making it available in every template page you make.
+
+## Gulp tasks
+- **gulp** will build the files inside the /dist folder and perform a watch. You are ready to start developing
+- **gulp server** will build the files inside the /dist folder and generate a localserver, among other functionalitites. More information on this feature can be found [here](https://www.browsersync.io/)
+- **gulp build** will build the files inside the /dist folder. No watcher.
+- **gulp clean** will clean the dist folder
+
+## Notice
+The final release state is not deployed yet. It will include image processing, minification & other validations.
---
+++ gulpfile.js
@@ -0,0 +1,144 @@
+// Include gulp & del
+var gulp = require('gulp'); 
+var del = require('del');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var addsrc = require('gulp-add-src');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var tap = require('gulp-tap');
+var copy = require('gulp-copy');
+var runSequence = require('run-sequence');
+var handlebars = require('handlebars');
+var gulpHandlebars = require('gulp-handlebars-html')(handlebars);
+var browserSync = require('browser-sync').create();
+
+var dirs = {
+    source: 'src',
+    release: 'dist',
+    components: 'src/components'
+}
+
+function handleError(err) {
+  console.log(err.toString());
+  this.emit('end');
+}
+
+// Clean
+gulp.task('clean', function (cb) {
+    del([dirs.release], cb);
+});
+
+// Copy files
+gulp.task('copy:html', function() {
+    return gulp.src(dirs.source+'/*.html')
+        .pipe(copy(dirs.release, {prefix: 1}));
+});
+gulp.task('copy:images', function() {
+    return gulp.src(dirs.source+'/images/*')
+        .pipe(copy(dirs.release, {prefix: 1}));
+});
+gulp.task('copy:scripts', function() {
+    return gulp.src([
+            /*  
+             * Add vendor scripts here
+             * 
+             * Example:
+             * dirs.source+'/vendor/jquery/dist/jquery.min.js', 
+             */
+        ]).pipe(copy(dirs.release, {prefix: 1}));
+});
+
+
+// Lint Tasks
+gulp.task('lint:before', function() {
+    return gulp.src(dirs.source+'/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+gulp.task('lint:after', ['minify'], function() {
+    return gulp.src([
+            dirs.release+'/js/built.min.js',
+            dirs.release+'/js/built.js'
+        ])
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Handlerbars task
+gulp.task('handlebars', function(){
+    var templateData = {},
+        options = {
+            partialsDirectory : [dirs.components]
+        },
+        filename;
+
+    return gulp.src(dirs.source+'/pages/*.hbs')
+        .pipe(gulpHandlebars(templateData, options))
+        .pipe(rename(function(path){
+             filename = path.basename
+         }))
+        .pipe(addsrc.prepend(dirs.components+'/_global/head.html'))
+        .pipe(addsrc.append(dirs.components+'/_global/foot.html'))
+        .pipe(concat('temp.html'))
+        .pipe(rename(function(path){
+            path.basename = filename
+        }))
+        .pipe(gulp.dest(dirs.release+'/pages'));
+});
+
+// Concatenate & Minify JS
+gulp.task('concat', function() {
+    return gulp.src(dirs.source+'/js/*.js')
+        .pipe(concat('built.js'))
+        .pipe(gulp.dest(dirs.release+'/js'));
+});
+gulp.task('minify', function() {
+    return gulp.src(dirs.release+'/js/built.js')
+        .pipe(rename('built.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(dirs.release+'/js'));
+});
+
+// Compile Sass
+gulp.task('sass', function() {
+    return gulp.src(dirs.source+'/css/main.scss')
+        .pipe(sass())
+        .on('error', handleError)
+        .pipe(gulp.dest(dirs.release+'/css'));
+});
+
+function watcher() {
+
+    gulp.watch(dirs.source+'/*.html', ['copy:html', 'handlebars']);
+    gulp.watch(dirs.components+'/**/*.hbs', ['handlebars']);
+    gulp.watch(dirs.source+'/pages/*.hbs', ['handlebars']);
+    gulp.watch(dirs.source+'/js/*.js', ['lint:before', 'concat']);
+    gulp.watch(dirs.source+'/css/*.scss', ['sass']);
+}
+
+// Browsersync
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: dirs.release
+        }
+    });
+
+    watcher();
+});
+
+// Watch
+gulp.task('watch', watcher);
+
+// Build and release tasks
+gulp.task('release', function(callback){
+    runSequence('build', ['minify', 'lint:after'], callback);
+}); 
+gulp.task('build', function(callback){
+    runSequence('clean', ['copy:images', 'handlebars', 'copy:html', 'copy:scripts', 'sass', 'lint:before', 'concat'], callback);
+}); 
+gulp.task('default', ['build', 'watch']);
